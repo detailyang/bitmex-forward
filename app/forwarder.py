42b16305eb11c09d9cedf981502a16586b4e7cb0
detailyang <detailyang@gmail.com>
2018-08-06 08:19:32 +0800

    feature: support rejected order

--- app/forwarder.py
+++ app/forwarder.py
@@ -111,6 +111,26 @@ async def process_cancel_order(channel, o, log):
     content = "%s: %s" % (channel, content)
 
     await log(title, content)
+
+async def process_rejected_order(channel, o, log):
+    symbol = o["symbol"]
+    price = o["price"]
+    side = o["side"]
+    ex_destination = o["exDestination"]
+    order_qty = o["orderQty"]
+    order_type = o["ordType"]
+    order_status = o["ordStatus"]
+    last_qty = o["lastQty"]
+    leaves_qty = o["leavesQty"]    
+    text = o['text']
+    stop_price = o["stopPx"]
+
+    title = "%s Order Rejected" % (order_type)
+    content = text
+
+    content = "%s: %s" % (channel, content)
+
+    await log(title, content)
     
 
 def forwarder(testnet, symbols, accounts, discordwebhook):
@@ -140,6 +160,10 @@ def forwarder(testnet, symbols, accounts, discordwebhook):
                 await process_restated_order(channel, o, log)
             elif exec_type == "TriggeredOrActivatedBySystem":
                 await process_trigger_order(channel, o, log)
+            elif exec_type == "Rejected":
+                await process_rejected_order(channel, o, log)
+            elif exec_type == "Funding":
+                continue
             else:
                 logger.warning("unknow order", json.dumps(o))
 
